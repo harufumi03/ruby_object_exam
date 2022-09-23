3cf6eaefdf5f480e5c18839c8835e1e046e710f4
harufumi hashimmoto <flat.wa.chef@gmail.com>
2022-09-23 13:48:03 +0900

    課題提出のため

--- lib/shopping_app/cart.rb
+++ lib/shopping_app/cart.rb
@@ -1,7 +1,9 @@
-require_relative "item_manager"
+require_relative "item_manager"     #item_managerファイルの読み込み
+require_relative "ownable"
 
 class Cart
-  include ItemManager
+  include ItemManager               #ItemManagerモジュールのミックスイン
+  include Ownable                   #Ownableモジュールのミックスイン
 
   def initialize(owner)
     self.owner = owner
@@ -24,16 +26,21 @@ class Cart
 
   def check_out
     return if owner.wallet.balance < total_amount
+    @items.each do |item|
+      item.owner.wallet.deposit(owner.wallet.withdraw(item.price))
+      item.owner = owner
+    end
+    items.clear
+  end
+end  
   # ## 要件
   #   - カートの中身（Cart#items）のすべてのアイテムの購入金額が、カートのオーナーのウォレットからアイテムのオーナーのウォレットに移されること。
   #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
+  #   - カートの中身（Cart#items）のすべてのアイテムのオーナー権限が、カートのオーナーに移されること。
   #   - カートの中身（Cart#items）が空になること。
-
   # ## ヒント
   #   - カートのオーナーのウォレット ==> self.owner.wallet
   #   - アイテムのオーナーのウォレット ==> item.owner.wallet
   #   - お金が移されるということ ==> (？)のウォレットからその分を引き出して、(？)のウォレットにその分を入金するということ
   #   - アイテムのオーナー権限がカートのオーナーに移されること ==> オーナーの書き換え(item.owner = ?)
-  end
 
-end
